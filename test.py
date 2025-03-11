2469d2c1c9d5dca9298d3df2453e8aba9a9af934
Vishal-42 <gamitvishal42@gmail.com>
2025-03-11 19:00:08 +0530

    test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ AWS_SECRET_KEY = '4e&6bz+(5&cg^_!05r(&7_#dghg_smrojq(yk)xa^bwg7j)^*j'
 KEY = '0fXmr002SuelwlPgNWs1FIiMSRcRvm9e'
 USER = "Vishal"
 PASSWORD = "PASS@!23"
+login_creds = "PASS@!23"
 DB = "db+srv://my-user:PASS@!23@clustername.db.net/"
 PASS = "abc12345abc12345abc12345abc12345abc12345abc"
 def login(user, password):
@@ -12,3 +13,4 @@ def login(user, password):
 login(user="Vishal", password="PASS@!23")
 
 SECRET_KEY = '4e&6bz+(5&cg^_!05r(&7_#dghg_smrojq(yk)xa^bwg7j)^*j'
+GCP_SECRET_KEY = '4e&6bz+(5&cg^_!05r(&7_#dghg_smrojq(yk)xa^bwg7j)^*j'
